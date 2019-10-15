645d3e9ac34fe99df97ebdabff523dc835163bb1
jlozovei <jlozovei@gmail.com>
2019-10-14 22:39:01 -0300

    add some data
    - social networks + blogging sources

---
+++ www/src/data/texts.js
@@ -0,0 +1,14 @@
+const texts = [
+  {
+    name: 'Medium',
+    slug: 'medium',
+    link: 'https://medium.com/@jlozovei'
+  },
+  {
+    name: 'dev.to',
+    slug: 'dev',
+    link: 'https://dev.to/jlozovei'
+  }
+];
+
+export default texts;
