f147457a8fe6d41de5aa0587051e6d4488136a6e
jlozovei <jlozovei@gmail.com>
2019-10-15 00:27:15 -0300

    Updates

---
+++ precache-manifest.030fcf571e3c070531a32c316d04bf7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce228d814894511f91543a76f35117ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "979f65d2c89c336dda99",
+    "url": "./static/css/main.3b87e51e.chunk.css"
+  },
+  {
+    "revision": "2901ea16eb12ae972adf",
+    "url": "./static/js/2.76edcacc.chunk.js"
+  },
+  {
+    "revision": "979f65d2c89c336dda99",
+    "url": "./static/js/main.bc1ba027.chunk.js"
+  },
+  {
+    "revision": "0e69335450e417386853",
+    "url": "./static/js/runtime-main.b0243dab.js"
+  },
+  {
+    "revision": "59d965e63d42406da7134545d1b24018",
+    "url": "./static/media/hello.59d965e6.png"
+  },
+  {
+    "revision": "7f6fbfd75ed753ed9eb98259f7c8659f",
+    "url": "./static/media/noise.7f6fbfd7.gif"
+  },
+  {
+    "revision": "e237f06f59f604ee291a0e74e27a889a",
+    "url": "./static/media/thinking.e237f06f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jlozovei/me/precache-manifest.3315dc35cdb6f6cc9f5b4b59a491c316.js"
+  "./precache-manifest.030fcf571e3c070531a32c316d04bf7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jlozovei/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
