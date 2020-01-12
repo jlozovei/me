705f06c3709caef9f672ea4c4018031e79ac48b7
jlozovei <jlozovei@gmail.com>
2020-01-12 02:37:27 -0300

    create new `Section` component

---
+++ www/src/components/Section/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledSection = styled.section`
+  position: relative;
+  padding: 2rem 0;
+
+  ul {
+    li {
+      list-style: inside disc;
+    }
+  }
+`;
