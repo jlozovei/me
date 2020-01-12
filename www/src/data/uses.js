62f20ecc5bdb30369662e61034148c87da21f264
jlozovei <jlozovei@gmail.com>
2020-01-12 02:37:40 -0300

    create new `/uses` page

---
+++ www/src/data/uses.js
@@ -0,0 +1,28 @@
+const uses = {
+  home: {
+    computer: 'Apple MacBook Pro 13-inch, 2017',
+    editor:
+      'Sublime Text 3 + <a href="https://packagecontrol.io/packages/Seti_UI" target="_blank" rel="noopener">Seti_UI</a> theme',
+    terminal:
+      'iTerm2, <a href="https://github.com/ohmyzsh/ohmyzsh" target="_blank" rel="noopener">Oh My Zsh</a> + <a>robbyrussell</a> theme',
+    browser: 'Google Chrome/Google Chrome Canary',
+    headphones: 'Marshall Monitor Bluetooth'
+  },
+
+  work: {
+    computer: 'Apple MacBook Pro 13-inch, 2018',
+    editor:
+      'Sublime Text 3 + <a href="https://packagecontrol.io/packages/Seti_UI" target="_blank" rel="noopener">Seti_UI</a> theme',
+    terminal:
+      'iTerm2, <a href="https://github.com/ohmyzsh/ohmyzsh" target="_blank" rel="noopener">Oh My Zsh</a> + <a>robbyrussell</a> theme',
+    browser: 'Google Chrome/Google Chrome Canary',
+    earphones: 'harman/kardon IE NC (in-ear)'
+  },
+
+  personal: {
+    phone: 'Apple iPhone 7',
+    notes: 'Bear'
+  }
+};
+
+export default uses;
--- www/src/routes/routes.js
+++ www/src/routes/routes.js
@@ -5,6 +5,7 @@ import { history } from './history';
 
 import Home from 'pages/Home';
 import Me from 'pages/Me';
+import Uses from 'pages/Uses';
 import NotFound from 'pages/NotFound';
 
 const Routes = () => (
@@ -13,6 +14,7 @@ const Routes = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/me" component={Me} />
+        <Route exact path="/uses" component={Uses} />
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
