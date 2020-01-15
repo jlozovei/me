c1c3f132ec4642f7b6d96cc86afbb5bb86db4e8c
jlozovei <jlozovei@gmail.com>
2020-01-14 21:52:50 -0300

    remove default section's list style

--- www/src/components/Section/styled.js
+++ www/src/components/Section/styled.js
@@ -3,10 +3,4 @@ import styled from 'styled-components';
 export const StyledSection = styled.section`
   position: relative;
   padding: 2rem 0;
-
-  ul {
-    li {
-      list-style: inside disc;
-    }
-  }
 `;
