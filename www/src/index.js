daa52f33024cbffaf1614e9031c2041545bf5a2d
jlozovei <jlozovei@gmail.com>
2020-01-15 23:51:51 -0300

    initialize react-ga

--- www/src/index.js
+++ www/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
-import { history } from 'routes/history';
+import history from 'routes/history';
 
 import App from './App';
 import './i18n';
+import isLocal from 'helpers/isLocal';
 
 import 'assets/css/app.css';
+
+!isLocal() && ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
 
 ReactDOM.render(
   <Router history={history}>
