10c8854b004cc33b9380ed0cf471db4f6c555f70
jlozovei <jlozovei@gmail.com>
2020-04-12 02:33:10 -0300

    remove old dev files

--- www/src/components/ListInline/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { StyledListInline } from './styled';
-
-const ListInline = ({ children, header }) => {
-  return <StyledListInline header={header}>{children}</StyledListInline>;
-};
-
-export default ListInline;
--- www/src/data/portfolio.js
+++
@@ -1,9 +0,0 @@
-const portfolio = [
-  {
-    name: 'Trópico',
-    slug: 'tropico',
-    live: 'https://tropico.tv'
-  }
-];
-
-export default portfolio;
