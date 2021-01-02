32a2fd7d0efa870c16842b9cae9323ee08d5ce4d
jlozovei <jlozovei@gmail.com>
2021-01-02 20:58:02 -0300

    feat: remove testing consoles from js

--- _webpack/index.js
+++ _webpack/index.js
@@ -3,13 +3,9 @@ import { menu } from './modules/menu';
 (() => {
   const app = {
     init() {
-      console.log('app init');
-
       menu.init();
     }
   };
 
   app.init();
 })();
-
-console.log('index');
