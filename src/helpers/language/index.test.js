cdb22802f1263c8c67a42af45c5b8a787dbc8bc8
jlozovei <jlozovei@gmail.com>
2021-10-25 18:22:29 -0300

    test: add async flag on helpers

--- src/helpers/language/index.test.js
+++ src/helpers/language/index.test.js
@@ -1,14 +1,14 @@
 import { language, isEn } from 'helpers/language';
 
 describe('helpers/language', () => {
-  it('language', () => {
+  it('language', async () => {
     expect(language()).toBe('en');
     expect(language('/en/about')).toBe('en');
     expect(language('/pt-br/sobre')).toBe('pt-br');
     expect(language('/es/sobre')).toBe('en');
   });
 
-  it('isEn', () => {
+  it('isEn', async () => {
     expect(isEn()).toBe(false);
     expect(isEn('en')).toBe(true);
     expect(isEn('pt-br')).toBe(false);
